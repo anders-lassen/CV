88f853b45906147de124721ab3e0df6abd7982a2
Anders Lassen <anders-lassen@outlook.dk>
2024-05-22 08:05:37 +0200

    init

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,138 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+// @refresh reset
+export default function Page() {
+    debugger
+    return (
+        <main className={styles.main}>
+            <div className={styles.description}>
+                <div className={styles.center}>
+                    <div id={styles.picture} className={styles.logo}></div>
+                </div>
+                <div>
+                    {/* Title */}
+                    <h1>Anders Rosenvinge Lassen</h1>
+                    <h3>Full stack Maestro: Looking for exciting projects... with a great motivation for developing new technologies!</h3>
+                    {/* Profile text */}
+                    <div className={styles.profiletext}>
+                        <p>Experienced, creative Developer with a knack for product and business development. Thrives on challenges and excels in independent work. Enjoys collaborative brainstorming and diving deep into details. Skilled in developing web technologies, API development and SaaS solutions. </p>
+                        <center><p>“I am Fueled by the thrill of development and driven to be a capital-S problemSolver.”</p></center>
+                    </div>
+                </div>
+                {/* Skillset (Full width) */}
+                <div className={styles.grid}>
+                    {/* Col1 - Programming skills */}
+                    <div className={styles.col}>
+                        <h4>Programming Skills:</h4>
+                        <ul className={`${styles.list} ${styles.list_disc}`}>
+                            <li>JavaScript / TypeScript</li>
+                            <li>Node.js</li>
+                            <li>RESTful API</li>
+                            <li>SQL / MySQL</li>
+                            <li>PHP</li>
+                            <li>Python</li>
+                            <li>GIT / GitHub / GitHub Actions</li>
+                            <li>Apache / Nginx</li>
+                            <li>HTML / CSS / SASS</li>
+                            <li>Website, Server & Database setup</li>
+                            <li>SaaS platform development</li>
+                        </ul>
+                    </div>
+                    {/* Col2 - Frameworks&Programs */}
+                    <div className={styles.col}>
+                        <h4>Frameworks & Programs</h4>
+                        <ul className={`${styles.list} ${styles.list_disc}`}>
+                            <li>React Framework</li>
+                            <li>AngularJS (Hybrid APP)</li>
+                            <li>Laravel Framework (PHP)</li>
+                            <li>Ionic Framework (Hybrid APP)</li>
+                            <li>Blender / Onshape (CAD)</li>
+                            <li>Visual Studio / VS Code</li>
+                            <li>AWS</li>
+                            <li>Android Studio</li>
+                            <li>Xcode</li>
+                            <li>Adobe Collection</li>
+                            <li>Microsoft package + Dynamics AX</li>
+                        </ul>
+                    </div>
+                </div>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+                <div className={styles.experience2}>
+                    <h4>Experience in developing systems with:</h4>
+                    <ul className={styles.list}>
+                        <li>Task/route management</li>
+                        <li>Time registration</li>
+                        <li>Order processing</li>
+                        <li>Purchase management</li>
+                        <li>Stock management</li>
+                        <li>Dashboard creation</li>
+                        <li>Calendar, Booking, Capacity</li>
+                        <li>CRM, ERP</li>
+                        <li>Documentation & Certification management</li>
+                        <li>Analytics; Interacitve reports, timereports, salesreports and stock development</li>
+                        <li>E-mail routing</li>
+                        <li>GPS Tracking; order positioning, timeregistration, images and quality checks</li>
+                        <li>Print & Reporting; Big Data</li>
+                        <li>Role Based Access</li>
+                        <li>QR-codes</li>
+                        <li>Event-driven notifications; SMS/Push/E-mail</li>
+                    </ul>
+                </div>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+                {/* Experience + Own Companies */}
+                <div className={styles.experience}>
+                    <h4>Experience</h4>
+                    <ul className={`${styles.list} ${styles.list_disc}`}>
+                        <li>Tracelink | Senior Fullstack Systems Developer | 2018 - 2024</li>
+                        <li>iBeacon.dk (SOTA Development) | Head of Development & Co-founder | 2014 - 2018</li>
+                        <li>Moonwalk Agency | Development manager | 2015 - 2017</li>
+                        <li>Husetsenergi | Supervisor & Implementation manager | 2013 - 2015</li>
+                        <li>SOTA Development IvS (Consultant agency) | Head of Tech & Co-founder | 2014 - 2018</li>
+                        <li>Butiksiden.dk (Advertising platform) | Co-founder | 2012 - 2015</li>
+                    </ul>
+                </div>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+                {/* Education */}
+                <div className={styles.education}>
+                    <h4>Education</h4>
+                    <ul className={styles.list}>
+                        <li>Syddansk Universitet | B.Sc., Major in Product Development and Innovation</li>
+                    </ul>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+                </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+                {/* Language */}
+                <div className={styles.language}>
+                    <h4>Language</h4>
+                    <ul className={styles.list}>
+                        <li>Danish, Verbal & Written | Fluent</li>
+                        <li>English, Verbal & Written | Fluent</li>
+                        <li>German, Verbal | Conversational level</li>
+                    </ul>
+                </div>
+
+                {/* Personal */}
+                <div className={styles.personal}>
+                    <h4>Personal</h4>
+                    <ul className={styles.list}><li>Spare time | 3D design and printing, programming projects, Dungeons and puzzles and hiking trips.</li></ul>
+                </div>
+                <div className={styles.about}>
+                    <h4>About</h4>
+                    <p>Personally I am outgoing, innovative and energetic. I have a proactive approach to projects and challenges, which is proven through my committed and independent work style.</p>
+                    <p>Through my work and education I learned how to create good architectural principles, structured design patterns and sound business logic. I've put them to the test in my own projects, where I've had the freedom to explore and experiment. Whether it's tinkering with code, 3D arts or craft projects, I'm all about diving in headfirst and delivering results that go above and beyond.</p>
+                    <center><p>“... I'm fueled by the thrill of development— the more intricate the challenge, the more exhilarating it becomes!”</p></center>
+                </div>
+
+                {/* Contact */}
+                <div className={styles.contact}>
+                    <h3>Anders Rosenvinge Lassen</h3>
+                    <h4>+45 3031 3408</h4>
+                    <h4>Anders-lassen@outlook.dk</h4>
+                    <h4><a href="https://dk.linkedin.com/in/anders-rosenvinge-lassen" target="_blank">https://dk.linkedin.com/in/anders-rosenvinge-lassen</a></h4>
+                </div>
+            </div>
+        </main>
+    );
 }
+
